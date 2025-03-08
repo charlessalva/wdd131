4f342763598dcefeaa4574b3f46cc33ec5bd063e
charlessalva <charlesnephiibascosalva@gmail.com>
2025-03-08 22:56:51 +0800

    made changes to conform with audit tool

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
-// Get the last modified date and format it
-const lastModifiedDate = new Date(document.lastModified);
-const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-
-document.getElementById("last-modified").textContent = "Last Modified: " + lastModifiedDate.toLocaleString('en-US', options);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("year").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = document.lastModified;
+});
